d767c9531262738151045334dcdcdd193f2eeafc
ajalabamise <ajalabamise@gmail.com>
2023-10-25 13:35:43 -0700

    first commit

---
+++ pages/footer.php
@@ -0,0 +1,36 @@
+<?php include 'pages/footer.php'; ?>
+<?php include 'footer.php'; ?>
+
+<!-- to attach the above to other files, we need change the html file to php file -->
+
+<!-- footer section -->
+<div class="footer">
+    <div class="box-container">
+        
+        <div class="box">
+            <h3>why choose us</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro qui dolores fugit similique aliquid tempore corrupti pariatur, voluptatem minima nostrum assumenda, ipsam quidem, quisquam dicta tenetur et ad error cumque!</p>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#">home</a>
+            <a href="#">about</a>
+            <a href="#">service</a>
+            <a href="#">plan</a>
+            <a href="#">contact</a>
+        </div>
+
+        <div class="box">
+            <h3>newsletter</h3>
+            <p>Subscribe us for latest updates</p>
+            <form action="">
+                <input type="email" placeholder="enter your email">
+                <input type="submit" class="btn" value="subscribe">
+            </form>
+        </div>
+
+
+    </div>
+</div>
+
